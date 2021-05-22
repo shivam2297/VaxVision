d9b875c8725bb2765e0f9fa45968d7dfa53aedec
Shivam <shivam2297@gmail.com>
2021-05-22 20:08:28 +0530

    added splash activity

---
+++ app/src/main/java/in/hideandseek/vaxvision/screens/splash/view/SplashActivity.java
@@ -0,0 +1,36 @@
+package in.hideandseek.vaxvision.screens.splash.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import in.hideandseek.vaxvision.R;
+import in.hideandseek.vaxvision.common.BaseActivity;
+import in.hideandseek.vaxvision.screens.home.view.HomeActivity;
+import in.hideandseek.vaxvision.screens.splash.presenter.SplashPresenterImpl;
+
+public class SplashActivity extends BaseActivity implements ISplashView {
+
+    private SplashPresenterImpl mPresenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mPresenter = new SplashPresenterImpl();
+        mPresenter.onViewBeingCreated(this);
+        mPresenter.startTimer();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mPresenter.onViewBeingDestroyed();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onSplashTimeOut() {
+        launchActivity(this, HomeActivity.class);
+    }
+}
